a46a9b4de522a51336c3fbd0a3dafb288efa0335
Carlos Felipe da Paixão <paixao.carlosfelipe@gmail.com>
2024-09-04 21:42:52 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float divQuebrada = 6f / 5;
+
+            Console.WriteLine(divQuebrada);
+
+            /*
+              
+
+
+            **** EXPLICAÇÃO USUARIO DIGITAR ****
+            
+            Console.WriteLine("Escreva seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Seu nome é: ");
+            Console.WriteLine(nome);
+            
+            
+              
+            **** EXPLICAÇÃO TIPAGEM VARIÁVEIS ****
+            
+            int anoNascimento = 1995;
+            string mesNascimento = "Julho";
+            float velocidade = 91.2f;
+            bool segundaFeira = true;
+
+
+            Console.WriteLine(anoNascimento);
+            Console.WriteLine(mesNascimento);
+            Console.WriteLine(velocidade);
+            Console.WriteLine(segundaFeira);
+
+            velocidade = 101.4f;
+            mesNascimento = "Agosto";
+
+            Console.WriteLine(velocidade);
+            Console.WriteLine(mesNascimento);
+
+                         
+            **** EXPLICAÇÃO VARIÁVEL DYNAMIC (VARIÁVEL QUE PODE SER ALTERADA) ****
+            
+            dynamic essaVarPodeAlterarTipo = "Irei mudar o tipo";
+
+            Console.WriteLine(essaVarPodeAlterarTipo);
+
+            essaVarPodeAlterarTipo = 121.1f;
+
+            Console.WriteLine(essaVarPodeAlterarTipo);
+
+
+            **** EXPLICAÇÃO CONSTANTES (VARIÁVEL QUE NUNCA É ALTERADA) ****
+            
+            const float PI = 3.14f;
+
+            */
+
+            Console.ReadLine();
+                
+            
+                
+        }
+    }
+}
